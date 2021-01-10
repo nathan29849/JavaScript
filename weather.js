df590b10116c9f052faad7b2b92f99d2dd254c84
phs960418@gmail.com <phs960418@gmail.com>
2021-01-11 04:07:20 +0900

    #3.9 Getting the Weather part Two

--- weather.js
+++ weather.js
@@ -1,6 +1,20 @@
+const weather = document.querySelector(".js-date");
+
 
 const API_KEY = "30d1b4cda0f5002de381146e2beeb1ee";
 const COORDS = 'coords';
+
+function getWeather(lat, lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response){ 
+        return response.json();
+    }).then(function(json){
+        console.log(json);
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
  function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -14,6 +28,7 @@ function handleGeoSuccess(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
   
 function handleGeoError() {
@@ -25,11 +40,13 @@ function askForCoords(){
 }
 
 function loadCoords(){
-    const loadedCords =  localStorage.getItem(COORDS);
-    if(loadedCords === null){
+    const loadedCoords =  localStorage.getItem(COORDS);
+    if(loadedCoords === null){
         askForCoords();
     } else {
         // getWeather
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
