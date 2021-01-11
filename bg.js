41ac41f06a83ec39b8dd4e9f1fa778495e27842b
phs960418@gmail.com <phs960418@gmail.com>
2021-01-12 04:04:30 +0900

    plz

--- bg.js
+++ bg.js
@@ -6,9 +6,9 @@ const IMG_NUMBER = 3;
 //     console.log("finished loading");
 // } // 만약 API에서 이 일을 하고 있었다면 필요했을 것임.
 
-const image_1 = new URL("https://user-images.githubusercontent.com/67811880/104209013-86d55700-5474-11eb-8a5a-480acce1d6e3.jpeg");
-const image_2 = new URL("https://user-images.githubusercontent.com/67811880/104209839-8ab5a900-5475-11eb-8960-c9e5ab0b5ecf.jpeg");
-const image_3 = new URL("https://user-images.githubusercontent.com/67811880/104210622-72925980-5476-11eb-9ba8-81dc73d67a12.jpeg");
+// const image_1 = new URL("https://user-images.githubusercontent.com/67811880/104209013-86d55700-5474-11eb-8a5a-480acce1d6e3.jpeg");
+// const image_2 = new URL("https://user-images.githubusercontent.com/67811880/104209839-8ab5a900-5475-11eb-8960-c9e5ab0b5ecf.jpeg");
+// const image_3 = new URL("https://user-images.githubusercontent.com/67811880/104210622-72925980-5476-11eb-9ba8-81dc73d67a12.jpeg");
 
 
 function paintImage(imgNumber){
